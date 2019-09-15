f4b8c75418fc19327edb6a8a8a95e32c60a0e40c
Yulli Chong <yc@Yullis-MBP.hsd1.il.comcast.net>
2019-09-15 15:45:03 -0500

    testing node

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('GA FINAL PROJECT');
-});
+router.get('/', (req, res) => {
+  res.send("GA PROJECT")
+})
 
 module.exports = router;
