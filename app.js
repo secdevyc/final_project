11200aaa4bf215264752741c00d6f6bca2ece572
Yulli Chong <yc@Yullis-MBP.hsd1.il.comcast.net>
2019-09-16 15:38:45 -0500

    update

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 
-var express = require('express');
+const express = require('express');
+const mongoose = require('mongoose')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+
 
 const port = process.env.PORT || 3000;
+const MONGODB_URI = "mongodb+srv://yulli:thisisapassword123@sei-lg8su.mongodb.net/test?retryWrites=true&w=majority"
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connection.once("open", () => {
+  console.log("Connected to Mongoose");
+})
+
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
---
+++ public/javascripts/app.js
@@ -0,0 +1,5 @@
+const app = angular.module("Fitbook", []);
+
+app.controller("MainController", function(){
+  this.hello = "oh hai";
+});
